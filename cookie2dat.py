1474876d3ec81f4a0398e56a3121fe5d763df659
Mo Zhou <cdluminate@gmail.com>
2018-12-30 08:38:48 +0000

    update cookie2dat

--- cookie2dat.py
+++ cookie2dat.py
@@ -9,32 +9,23 @@ import glob
 import os
 
 
-def delComment(lines: List[str], result: List[str], state: bool = False) -> List[str]:
-    '''
-    delete comments from the given list. Note, it only deletes the
-    first consecutive lines which starts with # . Any line starts with #
-    after that won't be changed.
-    '''
-    if not lines:
-        return result
-    else:
-        if not state:
-            if lines[0].startswith('#'):
-                return delComment(lines[1:], result, False)
-            else:
-                return delComment(lines[1:], result + [lines[0]], True)
-        else:
-            return delComment(lines[1:], result + [lines[0]], True)
-
-
 def cookie2dat(src: str) -> None:
     '''
     Read "cookie" and do preprocessing. :src: refers to source file path.
     '''
     dest = src.replace('.cookie', '.dat')
     with open(src, 'r') as fsrc, open(dest, 'w') as fdest:
-        fdest.writelines(delComment(fsrc.readlines(), [], False))
-    print(f'{__file__}: Converted {src} -> {dest}')
+        contents = fsrc.readlines()
+        while contents:
+            if contents[0].startswith('#'):
+                contents.pop(0)
+            else:
+                break
+        if contents:
+            fdest.writelines(contents)
+            print(f'Converted {src} -> {dest}')
+        else:
+            print(f'{src} is empty. skipped')
 
 
 if __name__ == '__main__':
