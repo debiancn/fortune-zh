2931a24239ced6f2f6628d019f77cd39a89f0aa3
Mo Zhou <cdluminate@gmail.com>
2018-12-31 07:29:38 +0000

    Update colorizing code for debian-reference.

--- foss.d/debian-reference.py
+++ foss.d/debian-reference.py
@@ -10,20 +10,29 @@ White   = lambda x: '\x1b[37;1m' + x + '\x1b[;m'
 Green   = lambda x: '\x1b[32;1m' + x + '\x1b[;m'
 Red     = lambda x: '\x1b[31;1m' + x + '\x1b[;m'
 Cyan    = lambda x: '\x1b[36;1m' + x + '\x1b[;m'
+cyan    = lambda x: '\x1b[36m' + x + '\x1b[;m'
 Yellow  = lambda x: '\x1b[33;1m' + x + '\x1b[;m'
-yellow  = lambda x: '\x1b[33;0m' + x + '\x1b[;m'
+yellow  = lambda x: '\x1b[33m' + x + '\x1b[;m'
+violet  = lambda x: '\x1b[35m' + x + '\x1b[;m'
 
 def colorize(line):
     if re.match('^\s*\$.*', line):
         return Green(line)
     elif re.match('^\s*#.*', line):
-        return Cyan(line)
+        return Red(line)
     else:
+        line = re.sub('(“.*?”)', '\x1b[35m\\1\x1b[;m', line)
+        line = re.sub('(".*?")', '\x1b[35m\\1\x1b[;m', line)
+        line = re.sub('(\'.*?\')', '\x1b[35m\\1\x1b[;m', line)
+        #line = re.sub('^(\s*[\d\.]*)', '\x1b[33;1m\\1\x1b[;m', line)
+        line = re.sub('(\w*?\(\d\))', '\x1b[34;1m\\1\x1b[m', line)
+        line = re.sub('(^\s*\*)', '\x1b[33;1m\\1\x1b[;m', line)
+        line = re.sub('(^\s*\+)', '\x1b[33;1m\\1\x1b[;m', line)
         line = re.sub('注意', '\x1b[33;1m注意\x1b[m', line)
         line = re.sub('小心', '\x1b[33;1m小心\x1b[m', line)
         line = re.sub('提示', '\x1b[36;1m提示\x1b[m', line)
         line = re.sub('警告', '\x1b[31;1m警告\x1b[m', line)
-        line = re.sub('^(\s*)([\d\.]*)', Yellow('\\1\\2'), line)
+        #line = re.sub('(\d*?)', '\x1b[36m\\1\x1b[m', line)
         return line
 
 
@@ -36,7 +45,7 @@ if __name__ == '__main__':
     for i, line in enumerate(lines):
         line = line.rstrip()
         if re.match(r'^\S+.*', line) and i > 0:
-            print('  --                        Osamu Aoki (青木修), Debian 参考手册（版本 2.73）')
+            print('                           -- Osamu Aoki (青木修), Debian 参考手册（版本 2.73）')
             print('%')
             print(White(line))
         else:
